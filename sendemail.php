de421364373fbf40050663c7eaf072c3d5c739ab
petarpejic97 <petar.pejic@outlook.com>
2020-06-27 13:52:20 +0200

    cvbxcvb

--- sendemail.php
+++ sendemail.php
@@ -1,6 +1,6 @@
 <?php
 
-    $recipient="pejicpetar1@gmail.com";
+    $recipient="pejicpetar1@gmail,.com";
     $subject="Form to email message";
     $sender=$_POST["sender"];
     $senderEmail=$_POST["email"];

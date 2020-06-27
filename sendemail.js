c441474eb393dbcdb772033723eecad3b79f9eea
petarpejic97 <petar.pejic@outlook.com>
2020-06-27 13:36:44 +0200

    asdasd

--- sendemail.js
+++ sendemail.js
@@ -2,9 +2,10 @@ function send(){
     email = $("#email").val();
     sender = $("#sender").val();
     message = $("#message").val();
+    console.log(email+sender+message)
     $.ajax({
         type:'POST',
-        url:'/sendemail.php',
+        url:'sendemail.php',
         data:{email:email,
             sender:sender,
             message:message},
--- sendemail.php
+++ sendemail.php
@@ -1,19 +1,17 @@
 <?php
 
-if($_POST["submit"]) {
     $recipient="petar.pejic@outlook.com";
     $subject="Form to email message";
     $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
+    $senderEmail=$_POST["email"];
     $message=$_POST["message"];
 
     $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>")
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-    
-}
+    echo "<p>Thank you! Your message has been sent.</p>";
+   
 
 ?>
